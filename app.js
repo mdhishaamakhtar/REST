d0a5e90bc535e63ef6f14ae87e9a4f2687306e64
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-23 17:08:32 +0530

    Update

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express=require('express');
-const app=express();
-const mongoose=require('mongoose');
-const bodyParser=require('body-parser');
-const cors=require('cors');
-require('dotenv/config');
+//jshint esversion:6
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
 
-
-const postRoute=require('./routes/post.js');
+const postRoute = require("./routes/post.js");
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/posts",postRoute);
-app.get("/",(req,res)=>{
-  res.send("We are on home. Go to /posts to see the data");
+app.use("/posts", postRoute);
+app.get("/", (req, res) => {
+    res.send("We are on home. Go to /posts to see the data");
 });
-mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true },()=>{console.log("Connected to DB")});
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+    console.log("Connected to DB");
+});
 
 app.listen(3000);
