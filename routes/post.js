b1475fbdd684104af2e91a79da9f87e540a0efe2
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-26 02:34:43 +0530

    Made few changes to make code better

--- routes/post.js
+++ routes/post.js
@@ -2,15 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../model/Post");
-router.get("/", async(_req, res) => {
+router.get("/", async (_req, res) => {
     try {
         const posts = await Post.find();
         res.json(posts);
     } catch (err) {
-        res.json({ message: err });
+        res.json({message: err});
     }
 });
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
@@ -19,36 +19,41 @@ router.post("/", async(req, res) => {
         const savedPost = await post.save();
         res.json(savedPost);
     } catch (err) {
-        res.json({ message: err });
+        res.json({message: err});
     }
 });
 //SPECIFIC POST
-router.get("/:postId", async(req, res) => {
+router.get("/:postId", async (req, res) => {
     try {
         const post = await Post.findById(req.params.postId);
         res.json(post);
     } catch (err) {
-        res.json({ message: err });
+        res.json({message: err});
     }
 });
 
 //DELETE POST
-router.delete("/:postId", async(req, res) => {
+router.delete("/:postId", async (req, res) => {
     try {
-        const removedPost = await Post.remove({ _id: req.params.postId });
+        const removedPost = await Post.findByIdAndDelete(req.params.postId);
         res.json(removedPost);
     } catch (err) {
-        res.json({ message: err });
+        res.json({message: err});
     }
 });
 
 //PATCH POST
-router.patch("/:postId", async(req, res) => {
+router.patch("/:postId", async (req, res) => {
     try {
-        const updatedPost = await Post.updateOne({ _id: req.params.postId }, { $set: { title: req.body.title, description: req.body.description } });
+        const updatedPost = await Post.updateOne({_id: req.params.postId}, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description
+            }
+        });
         res.json(updatedPost);
     } catch (err) {
-        res.json({ message: err });
+        res.json({message: err});
     }
 });
 module.exports = router;
